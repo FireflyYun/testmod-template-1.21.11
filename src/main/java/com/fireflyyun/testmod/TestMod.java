c656c781a62c32881f9b99bca468f8603d2dc19d
FireflyYun <fireflyyun@nwafu.edu.cn>
2026-03-19 20:53:33 +0800

    完成爆米花的添加

--- src/main/java/com/fireflyyun/testmod/TestMod.java
+++ src/main/java/com/fireflyyun/testmod/TestMod.java
@@ -19,7 +19,7 @@ public class TestMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		ModItems.registerModItems();
+		ModItems.initialize();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/com/fireflyyun/testmod/item/ModItems.java
+++ src/main/java/com/fireflyyun/testmod/item/ModItems.java
@@ -1,22 +1,27 @@
 package com.fireflyyun.testmod.item;
 
-
-import com.fireflyyun.testmod.TestMod;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
 
 public class ModItems {
-    public static final Item ICE_ETHER = registerItems("ice_ether", new Item(new Item.Settings()));
+    public static final Item POPCORN = register("popcorn",Item::new , new Item.Settings());
 
-    public static Item registerItems(String id, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(TestMod.MOD_ID, id), item);
+    public static Item register(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
+
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("testmod", path));
+
+        return Items.register(registryKey, factory, settings);
+
     }
 
-    public static void registerModItems() {
-        TestMod.LOGGER.info("Registering ModItems");
+
+
+    public static void initialize() {
+
     }
 }
